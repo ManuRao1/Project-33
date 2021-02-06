1b62fb91851df55cef92ad97aecc267eeb1fb70a
ManuRao1 <74971923+ManuRao1@users.noreply.github.com>
2021-02-06 22:56:24 +0530

    Add files via upload

---
+++ projecttemplate-31-main/projecttemplate-31-main/drops.js
@@ -0,0 +1,6 @@
+class drops {
+    constructor(x,y){
+        super(x,y,50,50);
+        
+    }
+}
---
+++ projecttemplate-31-main/projecttemplate-31-main/sketch.js
@@ -0,0 +1,19 @@
+var maxDrops=100;
+
+function preload(){
+    
+}
+
+function setup(){
+    var canvas = createCanvas(800,800);
+  
+   
+    
+}
+
+function draw(){
+    for(var i=0; i<maxDrops; i++){
+   drops.push(new createDrop(random(0,800), random(0,800))
+    }
+}   
+
